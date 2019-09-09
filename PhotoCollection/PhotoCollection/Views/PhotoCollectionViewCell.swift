c33b39314a936da8701b7cc7b05d735a3b5a2888
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-09 15:24:08 -0700

    Added NSlayout Constraints

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -11,6 +11,8 @@ import UIKit
 class PhotoCollectionViewCell: UICollectionViewCell {
 
     var photo: Photo?
+	var imageView: UIImageView!
+	var label: UILabel!
 	
 	func setUpSubViews() {
 		
@@ -18,8 +20,65 @@ class PhotoCollectionViewCell: UICollectionViewCell {
 		
 		let imageView = photo?.imageData
 		let label = photo?.title
+		addSubview(imageView)
+		addSubview(label)
 		
+		NSLayoutConstraint(item: imageView,
+						   attribute: .leading,
+						   relatedBy: .equal,
+						   toItem: self,
+						   attribute: .leading,
+						   multiplier: 1,
+						   constant: 4).isActive = true
 		
+		NSLayoutConstraint(item: imageView,
+						   attribute: .trailing,
+						   relatedBy: .equal,
+						   toItem: self,
+						   attribute: .trailing,
+						   multiplier: 1,
+						   constant: -4).isActive = true
+		
+		NSLayoutConstraint(item: imageView,
+						   attribute: .height,
+						   relatedBy: .equal,
+						   toItem: imageView,
+						   attribute: .width,
+						   multiplier: 1,
+						   constant: 0).isActive = true
+		
+		NSLayoutConstraint(item: label,
+						   attribute: .leading,
+						   relatedBy: .equal,
+						   toItem: self,
+						   attribute: .leading,
+						   multiplier: 1,
+						   constant: 8).isActive = true
+		
+		NSLayoutConstraint(item: label,
+						   attribute: .trailing,
+						   relatedBy: .equal,
+						   toItem: self,
+						   attribute: .trailing,
+						   multiplier: 1,
+						   constant: -6).isActive = true
+		
+		NSLayoutConstraint(item: label,
+						   attribute: .height,
+						   relatedBy: .equal,
+						   toItem: imageView,
+						   attribute: .width,
+						   multiplier: 1,
+						   constant: 0).isActive = true
+		
+		self.imageView = imageView
+		self.label = label
+		
+	}
+	
+	private func updateViews() {
+		imageView.image = photo.image
+		nameLabel.text = photo?.title
 	}
     
 }
