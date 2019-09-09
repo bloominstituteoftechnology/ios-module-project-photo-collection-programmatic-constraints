e8fb69156570da2bfc73679ec1c7dfc2cec54372
ladybeitel <ciarabeitel@gmail.com>
2019-09-09 17:23:34 -0400

    Set up PhotoCollectionViewCell programmatically. Create updateViews func.

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -9,8 +9,114 @@
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    private var imageView: UIImageView!
+    private var label: UILabel!
 
-    var photo: Photo?
+    var photo: Photo? {
+        didSet {
+            updateViews()
+        }
+    }
     
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubviews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUpSubviews()
+    }
+    
+    func setUpSubviews() {
+        // Create an instance
+        let imageView = UIImageView()
+        // Disable auto-resizing constraints
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        // Make it scale the image to fit (square)
+        imageView.contentMode = .scaleAspectFit
+        // Add subview to view
+        addSubview(imageView)
+        
+        // Constraits with NSLayoutConstraints
+        NSLayoutConstraint(item: imageView,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: self, // view
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: self, // view
+                           attribute: .leading,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: self, // view
+                           attribute: .trailing,
+                           multiplier: 1,
+                           constant: -4).isActive = true
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: imageView,
+                           attribute: .width,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+        // Set the thing so it's usable to the class
+        self.imageView = imageView
+        
+        // Create an instance
+        let label = UILabel()
+        // Disable auto-resizing constraints
+        label.translatesAutoresizingMaskIntoConstraints = false
+        // Align text
+        label.textAlignment = .center
+        // Add subview to view
+        addSubview(label)
+        
+        //Constraits with NSLayoutConstraints
+        NSLayoutConstraint(item: label,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: imageView,
+                           attribute: .bottom,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+        
+        NSLayoutConstraint(item: label,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: imageView,
+                           attribute: .leading,
+                           multiplier: 1,
+                           constant: 2).isActive = true
+        
+        NSLayoutConstraint(item: label,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: imageView,
+                           attribute: .trailing,
+                           multiplier: 1,
+                           constant: -2).isActive = true
+        
+        //set the thing so it's usable to the class
+        self.label = label
+    }
+    
+    func updateViews() {
+        guard let photo = photo else { return }
+        imageView.image = UIImage(data: photo.imageData)
+        label.text = photo.title
+    }
 }
