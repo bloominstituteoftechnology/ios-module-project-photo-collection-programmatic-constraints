71b194261ae6554497c10dff43ece434c6e5d2e9
Andrew Ruiz <nukedoom@gmail.com>
2019-09-09 15:20:51 -0700

    Completed label

--- PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -10,14 +10,15 @@ import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
 
+    private var imageView: UIImageView!
+    private var nameLabel: UILabel!
+    
     var photo: Photo?
     
-    private var imageView: UIImageView!
     
     func setUpSubviews() {
         
         // Create an image view that will display the UIImage that the photo.imageData contains.
-        
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
@@ -29,13 +30,23 @@ class PhotoCollectionViewCell: UICollectionViewCell {
         
         NSLayoutConstraint(item: imageView, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: -4).isActive = true
         
-        NSLayoutConstraint(item: imageView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 4).isActive = true
+        NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 4).isActive = true
         
         self.imageView = imageView
         
         //Create a label that will display the Photo's title.
-
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(label)
         
+        // TODO: Not sure if correct
+        NSLayoutConstraint(item: label, attribute: .bottom, relatedBy: .equal, toItem: imageView, attribute: .bottom, multiplier: 1, constant: 4).isActive = true
+        
+        NSLayoutConstraint(item: label, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: 2).isActive = true
+        
+        NSLayoutConstraint(item: label, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: -2).isActive = true
+        
+        self.nameLabel = label
     }
     
 }
