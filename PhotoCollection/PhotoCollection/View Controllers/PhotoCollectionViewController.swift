6bc5539e09efa6a38b3ae0116adbb7b3dda82dc7
ladybeitel <ciarabeitel@gmail.com>
2019-09-09 17:49:33 -0400

    Implement UICollectionViewDelegateFlowLayout methods: insetForSectionAt minimumInteritemSpacingForSectionAt sizeForItemAt

--- PhotoCollection/PhotoCollection/View Controllers/PhotoCollectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/PhotoCollectionViewController.swift
@@ -54,6 +54,8 @@ class PhotoCollectionViewController: UICollectionViewController {
         collectionView?.backgroundColor = backgroundColor
     }
     
+    
+    
     // MARK: - Navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -85,5 +87,22 @@ class PhotoCollectionViewController: UICollectionViewController {
         default:
             break
         }
+    }
+}
+
+extension PhotoCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let edgeInset = UIEdgeInsets(top: 4, left: 4, bottom: -4, right: -4)
+        return edgeInset
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        let spacing = CGFloat(integerLiteral: 5)
+        return spacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = CGSize(width: 50, height: 50)
+        return size
     }
 }
