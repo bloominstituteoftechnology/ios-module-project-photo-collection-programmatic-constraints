0d00bb42607f7f8f59c6251d82112d1e2f53eb46
Andrew Ruiz <nukedoom@gmail.com>
2019-10-07 16:02:33 -0700

    Completed photocollectionViewcell

--- PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -9,11 +9,104 @@
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
-
-    var photo: Photo?
     
-    func setUpSubViews() {
+    // MARK: - Properties
+    var photo: Photo? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    private var imageView: UIImageView!
+    private var titleLabel: UILabel!
+    
+    private func setUpSubViews() {
         
+        // MARK: - Views
+        let imageView = UIImageView()
+        let titleLabel = UILabel()
+        
+        // Resizing and repositioning elements
+        imageView.contentMode = .scaleAspectFit
+        titleLabel.textAlignment = .center
+        
+        // Adding subviews
+        addSubview(imageView)
+        addSubview(titleLabel)
+        
+        // Turning off automatic constraints
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Adding Constraints
+        
+        // Image
+        NSLayoutConstraint.activate([
+                   NSLayoutConstraint(item: imageView,
+                                      attribute: .top,
+                                      relatedBy: .equal,
+                                      toItem: imageView,
+                                      attribute: .bottom,
+                                      multiplier: 1,
+                                      constant: 4),
+                   
+                   NSLayoutConstraint(item: imageView,
+                                      attribute: .leading,
+                                      relatedBy: .equal,
+                                      toItem: self,
+                                      attribute: .leading,
+                                      multiplier: 1,
+                                      constant: 8),
+                   
+                   NSLayoutConstraint(item: imageView,
+                                      attribute: .trailing,
+                                      relatedBy: .equal,
+                                      toItem: self,
+                                      attribute: .trailing,
+                                      multiplier: 1,
+                                      constant: -8)
+               ])
+        
+        // Label
+        NSLayoutConstraint.activate([
+                   NSLayoutConstraint(item: titleLabel,
+                                      attribute: .top,
+                                      relatedBy: .equal,
+                                      toItem: imageView,
+                                      attribute: .bottom,
+                                      multiplier: 1,
+                                      constant: 4),
+                   
+                   NSLayoutConstraint(item: titleLabel,
+                                      attribute: .leading,
+                                      relatedBy: .equal,
+                                      toItem: self,
+                                      attribute: .leading,
+                                      multiplier: 1,
+                                      constant: 2),
+                   
+                   NSLayoutConstraint(item: titleLabel,
+                                      attribute: .trailing,
+                                      relatedBy: .equal,
+                                      toItem: self,
+                                      attribute: .trailing,
+                                      multiplier: 1,
+                                      constant: -2)
+               ])
+        
+        
+        // Adding local views to actual view
+        self.imageView = imageView
+        self.titleLabel = titleLabel
+    }
+    
+    func updateViews() {
+        
+        guard let title = photo?.title,
+            let photoImageData = photo?.imageData else { return }
+        
+        titleLabel.text = title
+        imageView.image = UIImage(data: photoImageData)
         
     }
     
