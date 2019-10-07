f1b38d3075de4def494e8d25497b1d74444d276a
Isvvc <lyons@tuta.io>
2019-10-07 13:58:00 -0600

    Implement setUpSubViews()

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -12,5 +12,78 @@ class PhotoCollectionViewCell: UICollectionViewCell {
 
     var photo: Photo?
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpSubviews()
+    }
+    
+    func setUpSubviews() {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint(item: imageView,
+                               attribute: .top,
+                               relatedBy: .equal,
+                               toItem: self,
+                               attribute: .top,
+                               multiplier: 1,
+                               constant: 4),
+            
+            NSLayoutConstraint(item: imageView,
+                               attribute: .leading,
+                               relatedBy: .equal,
+                               toItem: self,
+                               attribute: .leading,
+                               multiplier: 1,
+                               constant: 2),
+            
+            NSLayoutConstraint(item: imageView,
+                               attribute: .trailing,
+                               relatedBy: .equal,
+                               toItem: self,
+                               attribute: .trailing,
+                               multiplier: 1,
+                               constant: -2)
+        ])
+        
+        let label = UILabel()
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint(item: label,
+                               attribute: .top,
+                               relatedBy: .equal,
+                               toItem: imageView,
+                               attribute: .bottom,
+                               multiplier: 1,
+                               constant: 4),
+            
+            NSLayoutConstraint(item: label,
+                               attribute: .leading,
+                               relatedBy: .equal,
+                               toItem: imageView,
+                               attribute: .leading,
+                               multiplier: 1,
+                               constant: 2),
+            
+            NSLayoutConstraint(item: label,
+                               attribute: .trailing,
+                               relatedBy: .equal,
+                               toItem: imageView,
+                               attribute: .trailing,
+                               multiplier: 1,
+                               constant: 2)
+        ])
+    }
     
 }
