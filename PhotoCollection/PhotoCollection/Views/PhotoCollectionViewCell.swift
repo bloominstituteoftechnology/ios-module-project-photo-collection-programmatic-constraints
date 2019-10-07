652063813f02809356f57c42184ad076f5c041df
Wilma <kima_92@fastmail.com>
2019-10-07 16:29:06 -0400

    Implemented setUpSubViews() and updateViews() in PhotoCollectionViewCell.swift

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -9,8 +9,108 @@
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
-
-    var photo: Photo?
     
+    var photo: Photo?
+    private var imageView: UIImageView!
+    private var nameLabel: UILabel!
+    
+    private func setUpSubviews() {
+        
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        
+        let imageLabel = UILabel()
+        imageLabel.textAlignment = .center
+        
+        addSubview(imageLabel)
+        imageLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Activating all imageView's contraints
+        NSLayoutConstraint.activate([
+            
+            NSLayoutConstraint(item: imageView,
+                               attribute: .leading,
+                               relatedBy: .equal,
+                               toItem: self,
+                               attribute: .leading,
+                               multiplier: 1,
+                               constant: 2),
+            
+            NSLayoutConstraint(item: imageView,
+                               attribute: .trailing,
+                               relatedBy: .equal,
+                               toItem: self,
+                               attribute: .trailing,
+                               multiplier: 1,
+                               constant: -2),
+            
+            NSLayoutConstraint(item: imageView,
+                               attribute: .top,
+                               relatedBy: .equal,
+                               toItem: self,
+                               attribute: .top,
+                               multiplier: 1,
+                               constant: 4),
+            
+            NSLayoutConstraint(item: imageView,
+                               attribute: .width,
+                               relatedBy: .equal,
+                               toItem: imageView,
+                               attribute: .height,
+                               multiplier: 1,
+                               constant: 1)
+            
+        ])
+        self.imageView = imageView
+        
+        // Activating all imageLabel's contraints
+        NSLayoutConstraint.activate([
+            
+            NSLayoutConstraint(item: imageLabel,
+                               attribute: .leading,
+                               relatedBy: .equal,
+                               toItem: self,
+                               attribute: .leading,
+                               multiplier: 1,
+                               constant: 2),
+            
+            NSLayoutConstraint(item: imageLabel,
+                               attribute: .trailing,
+                               relatedBy: .equal,
+                               toItem: self,
+                               attribute: .trailing,
+                               multiplier: 1,
+                               constant: -2),
+            
+            NSLayoutConstraint(item: imageLabel,
+                               attribute: .top,
+                               relatedBy: .equal,
+                               toItem: imageView,
+                               attribute: .bottom,
+                               multiplier: 1,
+                               constant: 4),
+            
+            NSLayoutConstraint(item: imageLabel,
+                               attribute: .width,
+                               relatedBy: .equal,
+                               toItem: imageView,
+                               attribute: .width,
+                               multiplier: 1,
+                               constant: 1)
+            
+        ])
+        self.nameLabel = imageLabel
+    }
+    
+    private func updateViews(image: Data, title: String) {
+        guard let photoImage = photo?.imageData,
+            let title = photo?.title else { return }
+        
+        nameLabel.text = title
+        imageView.image = UIImage(data: photoImage)
+    }
     
 }
