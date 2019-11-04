66c417c62f5ca136c2e3dbe24eec243e8cfb3ce9
Jon Bash <jonbash@protonmail.com>
2019-11-04 15:26:38 -0800

    set up subviews in theme view controller

--- PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
@@ -9,16 +9,51 @@
 import UIKit
 
 class ThemeSelectionViewController: UIViewController {
+    
+    var darkButton: UIButton!
+    var blueButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpSubViews()
+    }
 
-    func selectDarkTheme() {
+    @objc func selectDarkTheme() {
         themeHelper?.setThemePreferenceToDark()
         dismiss(animated: true, completion: nil)
     }
     
-    func selectBlueTheme() {
+    @objc func selectBlueTheme() {
         themeHelper?.setThemePreferenceToBlue()
         dismiss(animated: true, completion: nil)
     }
     
     var themeHelper: ThemeHelper?
+    
+    func setUpSubViews() {
+        let spacingConstant: CGFloat = 8
+        
+        let selectLabel = UILabel()
+        view.addSubview(selectLabel)
+        selectLabel.translatesAutoresizingMaskIntoConstraints = false
+        selectLabel.text = "Select theme:"
+        selectLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: spacingConstant * 2).isActive = true
+        selectLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        darkButton = UIButton(type: .system)
+        view.addSubview(darkButton)
+        darkButton.addTarget(self, action: #selector(selectDarkTheme), for: .touchUpInside)
+        darkButton.setTitle("Dark", for: .normal)
+        darkButton.translatesAutoresizingMaskIntoConstraints = false
+        darkButton.topAnchor.constraint(equalTo: selectLabel.bottomAnchor, constant: spacingConstant).isActive = true
+        darkButton.trailingAnchor.constraint(equalTo: view.centerXAnchor, constant: -spacingConstant).isActive = true
+        
+        blueButton = UIButton(type: .system)
+        view.addSubview(blueButton)
+        blueButton.addTarget(self, action: #selector(selectBlueTheme), for: .touchUpInside)
+        blueButton.setTitle("Blue", for: .normal)
+        blueButton.translatesAutoresizingMaskIntoConstraints = false
+        blueButton.topAnchor.constraint(equalTo: darkButton.topAnchor).isActive = true
+        blueButton.leadingAnchor.constraint(equalTo: view.centerXAnchor, constant: spacingConstant).isActive = true
+    }
 }
