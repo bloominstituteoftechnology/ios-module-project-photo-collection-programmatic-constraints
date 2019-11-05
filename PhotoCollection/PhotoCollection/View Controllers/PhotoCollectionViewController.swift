127f041efa769e9cb33bc6d6ad1c47ac6bfe18bd
Brandi <blubrandi0@gmail.com>
2019-11-05 08:53:10 -0700

    Added UICollectionViewDelegateFlowLayout methods

--- PhotoCollection/PhotoCollection/View Controllers/PhotoCollectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/PhotoCollectionViewController.swift
@@ -13,6 +13,16 @@ class PhotoCollectionViewController: UICollectionViewController, UICollectionVie
     let photoController = PhotoController()
     let themeHelper = ThemeHelper()
     
+    private let itemsPerRiw: CGFloat = 3
+    
+    private let sectionInsets = UIEdgeInsets(top: 50.0,
+    left: 20.0,
+    bottom: 50.0,
+    right: 20.0)
+    
+    private let itemsPerRow: CGFloat = 3
+
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -36,14 +46,26 @@ class PhotoCollectionViewController: UICollectionViewController, UICollectionVie
         return cell
     }
     
-//    func collectionview(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewFlowLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        
-//        let itemsPerRow: CGFloat = 2
-//        
-//        
-//        
-//        
-//    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRiw
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return sectionInsets.left
+    }
+    
+
     
     private func setTheme() {
     
