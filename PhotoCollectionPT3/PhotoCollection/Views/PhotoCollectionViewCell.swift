52c9acd6457136d4ad787cac8f08a750ce998a3d
Jessie <threedaystoryllc@icloud.com>
2019-11-13 21:33:41 -0500

    PhotoCollectionViewCell set up

--- PhotoCollectionPT3/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollectionPT3/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -10,7 +10,56 @@ import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
 
-    var photo: Photo?
+    private var imageView: UIImageView!
+    private var titleLabel: UILabel!
     
+    var photo: Photo? {
+        didSet {
+            updateViews()
+        }
+    }
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpSubViews()
+    }
+    
+    func setUpSubViews() {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        addSubview(imageView)
+        
+        NSLayoutConstraint(item: imageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 4.0).isActive = true
+        NSLayoutConstraint(item: imageView, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1.0, constant: 4.0).isActive = true
+        NSLayoutConstraint(item: imageView, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1.0, constant: -4.0).isActive = true
+        NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal, toItem: self, attribute: .width, multiplier: 1.0, constant: 0.0).isActive = true
+        
+        self.imageView = imageView
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        addSubview(label)
+        
+        label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 4.0).isActive = true
+        label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 2.0).isActive = true
+        label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -2.0).isActive = true
+        
+        self.titleLabel = label
+    }
+    
+    func updateViews() {
+        if let photo = photo {
+            titleLabel.text = photo.title
+            
+            let image = UIImage(data: photo.imageData)
+            imageView.image = image
+        }
+    }
 }
