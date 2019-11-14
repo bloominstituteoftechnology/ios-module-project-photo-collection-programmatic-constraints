d7bbc290aecc511623e471a58d070aed6911737c
Denis Cedeno <denis.cedeno@gmail.com>
2019-11-13 21:50:52 -0500

    Initial starter project commit, created subView method, with image view and subview constraints

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -9,8 +9,86 @@
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
-
-    var photo: Photo?
     
+    var photo: Photo?
+    private var imageView: UIImageView!
+    private var titleLabel: UILabel!
+    
+    ////    Constrain these views to the cell programmatically using only the NSLayoutConstraint initializer. (Yes, the long one. No anchors yet. 😉)
+    ////    You will want to call this function only once. Otherwise you will end up adding image views and labels over and over again to the cell.
+
+    
+    func setUpViews() {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(data: photo!.imageData)
+        imageView.contentMode = .scaleAspectFit
+        addSubview(imageView)
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .leading,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .trailing,
+                           multiplier: 1,
+                           constant: -4).isActive = true
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: imageView,
+                           attribute: .width,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+        self.imageView = imageView
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        addSubview(label)
+        
+        NSLayoutConstraint(item: label,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: imageView,
+                           attribute: .bottom,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+        NSLayoutConstraint(item: label,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: nil,
+                           attribute: .leading,
+                           multiplier: 0,
+                           constant: 2).isActive = true
+        NSLayoutConstraint(item: label,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: nil,
+                           attribute: .trailing,
+                           multiplier: 0,
+                           constant: -2).isActive = true
+        
+        self.titleLabel = label
+    }
+    
+        ////    Create an updateViews() function that passes the information from the Photo to the image view and label. (If you need help turning Data into a UIImage look at UIImage's initializers in the documentation. Call this function sometime after the photo variable has been given a value from the cellForItem at method in the PhotosCollectionViewController. There are a couple places you could decide to do this.
     
 }
