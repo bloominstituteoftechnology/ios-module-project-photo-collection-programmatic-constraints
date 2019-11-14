e79470fb6fb931a931a20c2bbb37048b00c1d465
Joseph Rogers <joerogersss2016@gmail.com>
2019-11-13 22:33:41 -0600

    Cell File Complete

--- PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ThemeSelectionViewController: UIViewController {
+    
+     var themeHelper: ThemeHelper?
+    
+    override func viewDidLoad() {
+           setUpSubViews()
+       }
 
     func selectDarkTheme() {
         themeHelper?.setThemePreferenceToDark()
@@ -20,5 +26,25 @@ class ThemeSelectionViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    var themeHelper: ThemeHelper?
+  func setUpSubViews() {
+      let label = UILabel()
+      label.translatesAutoresizingMaskIntoConstraints = false
+      label.text = "Select the theme you would like to use"
+      label.textAlignment = .center
+      view.addSubview(label)
+      label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 20).isActive = true
+      label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: -20).isActive = true
+      label.heightAnchor.constraint(equalTo: label.heightAnchor, constant: 38).isActive = true
+      label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
+    
+    let button1 = UIButton()
+    button1.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(button1)
+    button1.setTitle("Dark", for: .normal)
+    button1.trailingAnchor.constraint(equalToSystemSpacingAfter: view.safeAreaLayoutGuide.trailingAnchor, multiplier: 20).isActive = true
+    button1.widthAnchor.constraint(equalToConstant: 187).isActive = true
+    button1.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    button1.topAnchor.constraint(equalToSystemSpacingBelow: label.bottomAnchor, multiplier: 0).isActive = true
+  }
+   
 }
--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -9,8 +9,82 @@
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
-
-    var photo: Photo?
     
+    weak var photoCellImageView: UIImageView!
+    weak var photoNameLabel: UILabel!
+
+    var photo: Photo? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    func updateViews() {
+        guard let photo = photo else {return}
+        photoCellImageView.image = UIImage(data: photo.imageData)
+        photoNameLabel.text = photo.title
+    }
+    
+    override init(frame: CGRect) {
+           super.init(frame: frame)
+           setUpSubViews()
+       }
+       
+       required init?(coder: NSCoder) {
+           super.init(coder: coder)
+           setUpSubViews()
+       }
+    
+    
+    func setUpSubViews() {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = true
+        imageView.contentMode = .scaleAspectFit
+        addSubview(imageView)
+        imageView.backgroundColor = .blue
+        
+        NSLayoutConstraint(item: imageView,
+                                 attribute: .top,
+                                 relatedBy: .equal,
+                                 toItem: self,
+                                 attribute: .top,
+                                 multiplier: 1,
+                                 constant: 4).isActive = true
+              
+              NSLayoutConstraint(item: imageView,
+                                 attribute: .leading,
+                                 relatedBy: .equal,
+                                 toItem: self,
+                                 attribute: .leading,
+                                 multiplier: 1,
+                                 constant: 4).isActive = true
+              
+              NSLayoutConstraint(item: imageView,
+                                 attribute: .trailing,
+                                 relatedBy: .equal,
+                                 toItem: self,
+                                 attribute: .trailing,
+                                 multiplier: 1,
+                                 constant: -4).isActive = true
+              
+              NSLayoutConstraint(item: imageView,
+                                 attribute: .height,
+                                 relatedBy: .equal,
+                                 toItem: imageView,
+                                 attribute: .width,
+                                 multiplier: 1,
+                                 constant: 0).isActive = true
+        
+        self.photoCellImageView = imageView
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(label)
+        label.text = photo?.title
+        label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 4).isActive = true
+        label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 2).isActive = true
+        label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -2).isActive = true
+        self.photoNameLabel = label
+    }
     
 }
