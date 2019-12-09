7fb54494afd519c5dbfa9acc745a334278499f5c
Chad Rutherford <chad_rutherford@outlook.com>
2019-12-09 13:58:43 -0500

    Finished Project

--- PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
@@ -9,16 +9,61 @@
 import UIKit
 
 class ThemeSelectionViewController: UIViewController {
-
-    func selectDarkTheme() {
+    
+    var themeHelper: ThemeHelper?
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Select the theme you would like to use:"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let darkButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Dark", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addTarget(self, action: #selector(selectDarkTheme), for: .touchUpInside)
+        return button
+    }()
+    
+    let blueButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Blue", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addTarget(self, action: #selector(selectBlueTheme), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupSubViews()
+    }
+    
+    private func setupSubViews() {
+        view.addSubview(titleLabel)
+        titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+    
+        view.addSubview(darkButton)
+        darkButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
+        darkButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: -30).isActive = true
+        
+        view.addSubview(blueButton)
+        blueButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
+        blueButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: 30).isActive = true
+    }
+    
+    @objc func selectDarkTheme() {
         themeHelper?.setThemePreferenceToDark()
         dismiss(animated: true, completion: nil)
     }
     
-    func selectBlueTheme() {
+    @objc func selectBlueTheme() {
         themeHelper?.setThemePreferenceToBlue()
         dismiss(animated: true, completion: nil)
     }
-    
-    var themeHelper: ThemeHelper?
 }
