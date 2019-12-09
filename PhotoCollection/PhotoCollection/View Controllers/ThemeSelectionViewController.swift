9c8ca4ef35fd099d979a1f64509bcb5a47f1ec67
Tyler Christian <tylerc71197@gmail.com>
2019-12-09 18:07:40 -0500

    Final Product

--- PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
@@ -10,15 +10,52 @@ import UIKit
 
 class ThemeSelectionViewController: UIViewController {
 
-    func selectDarkTheme() {
+    @objc func selectDarkTheme() {
         themeHelper?.setThemePreferenceToDark()
         dismiss(animated: true, completion: nil)
     }
     
-    func selectBlueTheme() {
+    @objc func selectBlueTheme() {
         themeHelper?.setThemePreferenceToBlue()
         dismiss(animated: true, completion: nil)
     }
     
     var themeHelper: ThemeHelper?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpSubViews()
+    }
+    
+    func setUpSubViews() {
+        let titlelabel = UILabel()
+        titlelabel.translatesAutoresizingMaskIntoConstraints = false
+        titlelabel.text = "Select the theme you would like to use"
+        titlelabel.textAlignment = .center
+        view.addSubview(titlelabel)
+        titlelabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        titlelabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        titlelabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        
+        
+        let darkButton = UIButton()
+        darkButton.translatesAutoresizingMaskIntoConstraints = false
+        darkButton.setTitle("Dark", for: .normal)
+        darkButton.setTitleColor(.systemBlue, for: .normal)
+        darkButton.addTarget(self, action: #selector(selectDarkTheme), for: .touchUpInside)
+        view.addSubview(darkButton)
+        darkButton.topAnchor.constraint(equalTo: titlelabel.bottomAnchor, constant: 10).isActive = true
+        darkButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: -30).isActive = true
+        
+        
+        
+        let blueButton = UIButton()
+        blueButton.translatesAutoresizingMaskIntoConstraints = false
+        blueButton.setTitle("Blue", for: .normal)
+        blueButton.setTitleColor(.systemBlue, for: .normal)
+        blueButton.addTarget(self, action: #selector(selectBlueTheme), for: .touchUpInside)
+        view.addSubview(blueButton)
+        blueButton.topAnchor.constraint(equalTo: titlelabel.bottomAnchor, constant: 10).isActive = true
+        blueButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: 30).isActive = true
+    }
 }
