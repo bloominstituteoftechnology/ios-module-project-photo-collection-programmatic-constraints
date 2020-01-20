3ea5738b8a19c7820e9e2f87816df2d2853e4d98
Kenny Dubroff <kennydubroff@hazystudios.com>
2020-01-19 22:33:19 -0800

    programmatically created cell's imageView and UILabel

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -9,8 +9,42 @@
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
-
-    var photo: Photo?
+    @IBOutlet var imageView: UIImageView!
     
+    
+    var photo: Photo? {
+        didSet {
+            setupViews()
+        }
+    }
+    
+    func setupViews() {
+        let imageView = UIImageView()
+        let label = UILabel()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        addSubview(imageView)
+        addSubview(label)
+        
+        //imageView constraints
+        let imageViewLeadingConstraint = NSLayoutConstraint(item: imageView, attribute: .leading, relatedBy: .equal, toItem: safeAreaLayoutGuide, attribute: .leading, multiplier: 1, constant: 4)
+        let imageViewTrailingConstraint = NSLayoutConstraint(item: imageView, attribute: .trailing, relatedBy: .equal, toItem: safeAreaLayoutGuide, attribute: .trailing, multiplier: 1, constant: 4)
+        let imageViewTopConstraint = NSLayoutConstraint(item: imageView, attribute: .top, relatedBy: .equal, toItem: safeAreaLayoutGuide, attribute: .top, multiplier: 1, constant: 4)
+        
+        //label constraints
+        let labelLeadingConstraint = NSLayoutConstraint(item: label, attribute: .leading, relatedBy: .equal, toItem: imageView, attribute: .leading, multiplier: 1, constant: 0)
+        let labelTraiingConstraint = NSLayoutConstraint(item: label, attribute: .trailing, relatedBy: .equal, toItem: imageView, attribute: .trailing, multiplier: 1, constant: 0)
+        let labelBottomConstraint = NSLayoutConstraint(item: label, attribute: .bottom, relatedBy: .equal, toItem: safeAreaLayoutGuide, attribute: .bottom, multiplier: 1, constant: 4)
+        NSLayoutConstraint.activate([
+            imageViewLeadingConstraint,
+            imageViewTrailingConstraint,
+            imageViewTopConstraint,
+            labelLeadingConstraint,
+            labelTraiingConstraint,
+            labelBottomConstraint
+        ])
+    }
     
 }
