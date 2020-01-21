75d1118c30a3e7f7e3b4a51cef40c1c75973d319
Tobi Tselios <tobi.tselios@gmail.com>
2020-01-21 13:04:11 +0000

    PhotoCollectionViewCell setUpSubviews method done

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -12,5 +12,87 @@ class PhotoCollectionViewCell: UICollectionViewCell {
 
     var photo: Photo?
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpSubviews()
+    }
+    
+    func setUpSubviews() {
+        // Image
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(imageView)
+        
+        let imageViewTopConstraint = NSLayoutConstraint(item: imageView,
+                                                        attribute: .top,
+                                                        relatedBy: .equal,
+                                                        toItem: self,
+                                                        attribute: .top,
+                                                        multiplier: 1,
+                                                        constant: 4)
+        
+        let imageVewLeadingConstraint = NSLayoutConstraint(item: imageView,
+                                                           attribute: .leading,
+                                                           relatedBy: .equal,
+                                                           toItem: self,
+                                                           attribute: .leading,
+                                                           multiplier: 1,
+                                                           constant: 4)
+        
+        let imageViewTrailingConstraint = NSLayoutConstraint(item: imageView,
+                                                             attribute: .trailing,
+                                                             relatedBy: .equal,
+                                                             toItem: self,
+                                                             attribute: .trailing,
+                                                             multiplier: 1,
+                                                             constant: -4)
+        
+        let imageViewHeightConstraint = NSLayoutConstraint(item: imageView,
+                                                           attribute: .height,
+                                                           relatedBy: .equal,
+                                                           toItem: imageView,
+                                                           attribute: .width,
+                                                           multiplier: 0.75,
+                                                           constant: 0)
+        
+        // Label
+        
+        let label = UILabel()
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(label)
+        
+        let labelTopConstraint = NSLayoutConstraint(item: label,
+                                                    attribute: .top,
+                                                    relatedBy: .equal,
+                                                    toItem: self,
+                                                    attribute: .top,
+                                                    multiplier: 1,
+                                                    constant: 4)
+        
+        let labelLeadingConstraint = NSLayoutConstraint(item: label,
+                                                        attribute: .leading,
+                                                        relatedBy: .equal,
+                                                        toItem: self,
+                                                        attribute: .leading,
+                                                        multiplier: 1,
+                                                        constant: 4)
+        
+        let labelTrailingConstraint = NSLayoutConstraint(item: label,
+                                                         attribute: .trailing,
+                                                         relatedBy: .equal,
+                                                         toItem: self,
+                                                         attribute: .trailing,
+                                                         multiplier: 1,
+                                                         constant: -4)
+    
+       NSLayoutConstraint.activate([imageViewTopConstraint, imageVewLeadingConstraint, imageViewTrailingConstraint, imageViewHeightConstraint, labelTopConstraint, labelLeadingConstraint, labelTrailingConstraint])
+    }
     
 }
