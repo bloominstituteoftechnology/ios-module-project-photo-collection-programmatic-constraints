04873aeaf7e66e3db10345345e6b97f497aefd4c
sal562 <salmanamer@gmail.com>
2020-01-29 05:10:51 -0800

    finished settingUp CustomCell

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -9,8 +9,60 @@
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
-
-    var photo: Photo?
     
+    var imageView: UIImageView!
+    var nameLabel: UILabel!
+    
+    
+
+    var photo: Photo? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpSubviews()
+    }
+    
+    func updateViews() {
+//        imageview.image = photo?.imageData
+        
+        
+    }
+    
+    func setUpSubviews() {
+        // add imageView
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        addSubview(imageView)
+        self.imageView = imageView
+        
+        // constraints for imageView
+        
+        imageView.topAnchor.constraint(equalTo: topAnchor, constant: 4).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 4).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -4).isActive = true
+        imageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 1, constant: 0).isActive = true
+        
+        // add labe below centered
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        addSubview(label)
+        self.nameLabel = label
+        
+        //constraints for label
+        label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 4).isActive = true
+        label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 2).isActive = true
+        label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -2).isActive = true
+    }
     
 }
