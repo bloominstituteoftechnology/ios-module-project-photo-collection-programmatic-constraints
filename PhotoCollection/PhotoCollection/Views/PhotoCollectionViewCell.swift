bd609957f3dcc5945ddab40dea8e5c078fb43dee
Nick Nguyen <ptnguyen1901@gmail.com>
2020-02-17 15:47:37 -0500

    Constraint label and image for the cell

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -10,7 +10,94 @@ import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
 
-    var photo: Photo?
+    var photo: Photo? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    private func updateView() {
+        if let photo = photo {
+            imageView.image = UIImage(data: photo.imageData)
+            photoLabel.text = photo.title
+        }
+        
+    }
+    
+    private var imageView: UIImageView!
+    private var photoLabel: UILabel!
     
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpSubviews()
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    private func setUpSubviews() {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        addSubview(imageView)
+        
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .bottom,
+                           relatedBy: .equal,
+                           toItem: self, attribute: .bottom,
+            multiplier: 1,
+            constant: -40).isActive = true
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute:.leading ,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .leading,
+                           multiplier: 1,
+                           constant: 8).isActive = true
+        
+        
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .trailing,
+                           multiplier: 1,
+                           constant: -8).isActive = true
+        
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: 8).isActive = true
+        
+        self.imageView = imageView
+        
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        addSubview(label)
+        
+        NSLayoutConstraint(item: label, attribute: .top, relatedBy: .equal, toItem: imageView, attribute: .bottom, multiplier: 1, constant: 4).isActive = true
+        NSLayoutConstraint(item: label, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: 8).isActive = true
+        NSLayoutConstraint(item: label, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: -8).isActive = true
+        NSLayoutConstraint(item: label, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: -4).isActive = true
+        
+        
+        self.photoLabel = label
+        
+        
+    }
 }
