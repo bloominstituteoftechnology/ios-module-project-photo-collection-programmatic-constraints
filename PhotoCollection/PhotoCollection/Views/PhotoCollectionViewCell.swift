9e44673488ee6cee3e7a69c8ef0d1628e1ff07ca
scottharris86 <scottharrisdev@gmail.com>
2020-02-17 16:38:26 -0500

    finished programmatic constraints for collection view cell

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -10,7 +10,93 @@ import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
 
-    var photo: Photo?
+    var photo: Photo? {
+        didSet {
+            updateViews()
+        }
+    }
+    let imageView = UIImageView()
+    let titleLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupSubviews()
+    }
+    
+    func updateViews() {
+        if let photo = photo {
+            imageView.image = UIImage(data: photo.imageData)
+            titleLabel.text = photo.title
+        }
+    }
+    
+    func setupSubviews() {
+        configureImageView()
+        configureTitleLabel()
+    }
+    
+    func configureImageView() {
+        addSubview(imageView)
+        imageView.contentMode = .scaleAspectFit
+        NSLayoutConstraint(item: imageView,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .leading,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .trailing,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .bottom,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .bottom,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+    }
+    
+    func configureTitleLabel() {
+        addSubview(titleLabel)
+        titleLabel.contentMode = .center
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: titleLabel,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .leading,
+                           multiplier: 1,
+                           constant: 2).isActive = true
+        NSLayoutConstraint(item: titleLabel,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .trailing,
+                           multiplier: 1,
+                           constant: -2).isActive = true
+        
+    }
     
     
 }
