77e0b28f55256039afa1b563c988b3c918b6e8a8
knopptimism <56057392+knopptimism@users.noreply.github.com>
2020-02-17 16:15:31 -0800

    UICollectiionViewFlow done.

--- PhotoCollection/PhotoCollection/View Controllers/PhotoCollectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/PhotoCollectionViewController.swift
@@ -98,7 +98,7 @@ extension PhotoDetailViewController: UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width, height: <#T##CGFloat#>)
+        return CGSize(width: collectionView.frame.width, height: 150)
     }
     
 }
