8cbfcf6920d53fc23ec588a8acac3f35f1302f0a
Hector L <hectoremil21@hotmail.com>
2020-03-16 18:31:04 -0400

    theme stuff

--- PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
@@ -10,15 +10,35 @@ import UIKit
 
 class ThemeSelectionViewController: UIViewController {
 
-    func selectDarkTheme() {
+    @objc func selectDarkTheme() {
         themeHelper?.setThemePreferenceToDark()
         dismiss(animated: true, completion: nil)
     }
     
-    func selectBlueTheme() {
+    @objc func selectBlueTheme() {
         themeHelper?.setThemePreferenceToBlue()
         dismiss(animated: true, completion: nil)
     }
     
     var themeHelper: ThemeHelper?
+    
+    private func setUpSubviews() {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Select the theme you would like to use:"
+        
+        let darkButton = UIButton()
+        darkButton.translatesAutoresizingMaskIntoConstraints = false
+        darkButton.setTitle("Dark", for: .normal)
+        darkButton.addTarget(self, action: #selector(selectDarkTheme), for: .touchUpInside)
+        view.addSubview(darkButton)
+        
+        let blueButton = UIButton()
+        blueButton.translatesAutoresizingMaskIntoConstraints = false
+        blueButton.setTitle("Blue", for: .normal)
+        blueButton.addTarget(self, action: #selector(selectBlueTheme), for: .touchUpInside)
+        view.addSubview(blueButton)
+        
+        
+    }
 }
