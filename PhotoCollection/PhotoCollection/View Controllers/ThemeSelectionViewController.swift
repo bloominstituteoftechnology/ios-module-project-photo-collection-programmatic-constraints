f90b202bcf8aca1cd200d0c0412631dff29db1de
Hector L <hectoremil21@hotmail.com>
2020-03-16 18:47:25 -0400

    Done. Don't know why buttons in theme helper aren't showing

--- PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
@@ -22,10 +22,17 @@ class ThemeSelectionViewController: UIViewController {
     
     var themeHelper: ThemeHelper?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpSubviews()
+    }
+    
     private func setUpSubviews() {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Select the theme you would like to use:"
+        label.textAlignment = .center
+        view.addSubview(label)
         
         let darkButton = UIButton()
         darkButton.translatesAutoresizingMaskIntoConstraints = false
@@ -39,6 +46,23 @@ class ThemeSelectionViewController: UIViewController {
         blueButton.addTarget(self, action: #selector(selectBlueTheme), for: .touchUpInside)
         view.addSubview(blueButton)
         
+        // MARK: - Constraints
         
+        // label
+        label.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        label.topAnchor.constraint(equalTo: view.topAnchor, constant: 30).isActive = true
+        
+        // darkbutton
+        darkButton.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20).isActive = true
+        darkButton.leadingAnchor.constraint(equalTo: label.leadingAnchor, constant: 20).isActive = true
+        darkButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        darkButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        // blueButton
+        blueButton.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20).isActive = true
+        blueButton.trailingAnchor.constraint(equalTo: label.trailingAnchor, constant: -20).isActive = true
+        blueButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
     }
 }
