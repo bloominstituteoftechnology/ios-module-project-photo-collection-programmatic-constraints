e64b3f60329c48086c20e53c937768a52346520b
Wyatt Harrell <wyattaharrell@gmail.com>
2020-03-16 16:15:04 -0400

    theme helper buttons and label created

--- PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
@@ -10,15 +10,47 @@ import UIKit
 
 class ThemeSelectionViewController: UIViewController {
 
-    func selectDarkTheme() {
+    @objc func selectDarkTheme() {
         themeHelper?.setThemePreferenceToDark()
         dismiss(animated: true, completion: nil)
     }
     
-    func selectBlueTheme() {
+    @objc func selectBlueTheme() {
         themeHelper?.setThemePreferenceToBlue()
         dismiss(animated: true, completion: nil)
     }
     
-    var themeHelper: ThemeHelper?
+    var themeHelper: ThemeHelper? {
+        didSet {
+            setUpSubviews()
+        }
+    }
+    
+    func setUpSubviews() {
+        let label = UILabel()
+        label.contentMode = .center
+        label.text = "Select the theme you would like to use:"
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        
+        
+        
+        
+        
+        let blueButton = UIButton(type: .system)
+        blueButton.setTitle("Blue", for: .normal)
+        blueButton.addTarget(self, action: #selector(selectBlueTheme), for: .touchUpInside)
+        
+        
+        
+        
+        let darkButton = UIButton(type: .system)
+        darkButton.setTitle("Dark", for: .normal)
+        darkButton.addTarget(self, action: #selector(selectDarkTheme), for: .touchUpInside)
+        
+        
+        
+        
+    }
 }
