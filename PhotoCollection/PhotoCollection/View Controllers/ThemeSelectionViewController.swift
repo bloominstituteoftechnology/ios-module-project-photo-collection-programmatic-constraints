8126b4787aa1dd3a26b5a40011cf9813df30b89a
Claudia Maciel <clc_80@yahoo.com>
2020-04-08 20:12:06 -0700

    ThemeSelectionViewController programmatic UI Set Up

--- PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
@@ -10,15 +10,73 @@ import UIKit
 
 class ThemeSelectionViewController: UIViewController {
 
-    func selectDarkTheme() {
+    // MARK: - Properties
+    var themeHelper: ThemeHelper?
+    var titleLabel: UILabel!
+    var subTitleLabel: UILabel!
+    
+    // MARK: - Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpSubviews()
+    }
+    
+    @objc func selectDarkTheme() {
         themeHelper?.setThemePreferenceToDark()
         dismiss(animated: true, completion: nil)
     }
     
-    func selectBlueTheme() {
+    @objc func selectBlueTheme() {
         themeHelper?.setThemePreferenceToBlue()
         dismiss(animated: true, completion: nil)
     }
     
-    var themeHelper: ThemeHelper?
+    func setUpSubviews() {
+        //Title
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "Photo Collection"
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 30)
+        view.addSubview(titleLabel)
+
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
+        self.titleLabel = titleLabel
+        
+        //Subtitle
+        let subTitleLabel = UILabel()
+        subTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subTitleLabel.text = "Select the theme you want to use."
+        view.addSubview(subTitleLabel)
+        
+        subTitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
+        subTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        self.subTitleLabel = subTitleLabel
+        
+        //Dark Button
+        let darkButton = UIButton(type: .system)
+        darkButton.translatesAutoresizingMaskIntoConstraints = false
+        darkButton.setTitle("Dark Theme", for: .normal)
+        darkButton.addTarget(self, action: #selector(selectDarkTheme), for: .touchUpInside)
+        view.addSubview(darkButton)
+        
+        darkButton.trailingAnchor.constraint(equalTo: view.centerXAnchor, constant: -20).isActive = true
+        darkButton.topAnchor.constraint(equalTo: subTitleLabel.bottomAnchor, constant: 40).isActive = true
+        
+        //Blue Button
+        let blueButton = UIButton(type: .system)
+        blueButton.translatesAutoresizingMaskIntoConstraints = false
+        blueButton.setTitle("Blue Theme", for: .normal)
+        blueButton.addTarget(self, action: #selector(selectBlueTheme), for: .touchUpInside)
+        view.addSubview(blueButton)
+        
+        blueButton.leadingAnchor.constraint(equalTo: view.centerXAnchor, constant: 20).isActive = true
+        blueButton.topAnchor.constraint(equalTo: subTitleLabel.bottomAnchor, constant: 40).isActive = true
+        
+    }
+    
+    
 }
