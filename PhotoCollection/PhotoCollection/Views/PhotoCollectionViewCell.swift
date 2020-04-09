4e1fa94bc89af8145d8bafc8ae1e597edcaf1f8a
Breanna Young <bree.j.32@gmail.com>
2020-04-08 23:18:33 -0400

    creating updateViews and setupSubviews methods

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -9,8 +9,69 @@
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    private var imageView = UIImageView()
+    private var photoLabel = UILabel()
 
     var photo: Photo?
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpSubviews()
+    }
+    
+    private func setUpSubviews() {
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        addSubview(imageView)
+        
+        
+        // Y position
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+        
+        // Leading / X position
+        
+        let imageLeadingContraint = NSLayoutConstraint(item: imageView,
+                                                       attribute: .leading,
+                                                       relatedBy: .equal,
+                                                       toItem: self,
+                                                       attribute: .leading,
+                                                       multiplier: 1,
+                                                       constant: 4).isActive = true
+        
+        // width / negative / left / trailing
+        
+        let imageWidthConstraint = NSLayoutConstraint(item: imageView,
+                                                      attribute: .trailing,
+                                                      relatedBy: .equal,
+                                                      toItem: self,
+                                                      attribute: .trailing,
+                                                      multiplier: 1,
+                                                      constant: -4).isActive = true
+        
+        // height
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: imageView,
+                           attribute: .width,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+    }
     
 }
