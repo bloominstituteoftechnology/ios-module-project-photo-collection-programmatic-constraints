173ab972b6916dc73ba3dddd0f31977b8f64b5e8
Pravin Patel <patelpra337@gmail.com>
2020-04-08 22:33:41 -0500

    Update ViewCell and ViewController

--- PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
@@ -10,15 +10,41 @@ import UIKit
 
 class ThemeSelectionViewController: UIViewController {
 
-    func selectDarkTheme() {
+    @objc func selectDarkTheme() {
         themeHelper?.setThemePreferenceToDark()
         dismiss(animated: true, completion: nil)
     }
     
-    func selectBlueTheme() {
+    @objc func selectBlueTheme() {
         themeHelper?.setThemePreferenceToBlue()
         dismiss(animated: true, completion: nil)
     }
     
     var themeHelper: ThemeHelper?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupSubViews()
+    }
+    
+    private func setupSubViews() {
+        let label = UILabel()
+        label.text = "Select a theme"
+        label.textAlignment = .center
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        let darkButton = UIButton(type: .system)
+        let blueButton = UIButton(type: .system)
+        
+        darkButton.setTitle("Dark Theme", for: .normal)
+        blueButton.setTitle("Blue Theme", for: .normal)
+        darkButton.addTarget(self, action: #selector(selectDarkTheme), for: .touchUpInside)
+        blueButton.addTarget(self, action: #selector(selectBlueTheme), for: .touchUpInside)
+        
+        
+        
+        
+        
+    }
 }
