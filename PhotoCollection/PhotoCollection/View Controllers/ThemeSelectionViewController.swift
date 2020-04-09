9d8d8620337176fb9de0eabdfe76c98d57ff3502
Kevin Stewart <kistewart419@gmail.com>
2020-04-08 23:44:23 -0400

    MVP

--- PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
@@ -31,85 +31,36 @@ class ThemeSelectionViewController: UIViewController {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Select a theme"
-        view.addSubview(label)
+        self.view.addSubview(label)
+        
+        label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        label.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 30).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        label.widthAnchor.constraint(equalToConstant: 200).isActive = true
         
         let darkButton = UIButton()
         darkButton.translatesAutoresizingMaskIntoConstraints = false
         darkButton.setTitle("Dark", for: .normal)
+        darkButton.backgroundColor = .darkGray
         darkButton.addTarget(self, action: #selector(selectDarkTheme), for: .touchUpInside)
-        view.addSubview(darkButton)
+        self.view.addSubview(darkButton)
+        
+        darkButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -50).isActive = true
+        darkButton.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 100).isActive = true
+        darkButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        darkButton.widthAnchor.constraint(equalToConstant: 70).isActive = true
         
         let blueButton = UIButton()
         blueButton.translatesAutoresizingMaskIntoConstraints = false
         blueButton.setTitle("Blue", for: .normal)
+        blueButton.backgroundColor = .blue
         blueButton.addTarget(self, action: #selector(selectBlueTheme), for: .touchUpInside)
-        view.addSubview(blueButton)
+        self.view.addSubview(blueButton)
         
-        NSLayoutConstraint(item: label,
-                           attribute: .top,
-                           relatedBy: .equal,
-                           toItem: label.safeAreaLayoutGuide,
-                           attribute: .top,
-                           multiplier: 1,
-                           constant: 25).isActive = true
-        NSLayoutConstraint(item: label,
-                           attribute: .leading,
-                           relatedBy: .equal,
-                           toItem: label.safeAreaLayoutGuide,
-                           attribute: .leading,
-                           multiplier: 1,
-                           constant: 50).isActive = true
-        NSLayoutConstraint(item: label,
-                           attribute: .trailing,
-                           relatedBy: .equal,
-                           toItem: label.safeAreaLayoutGuide,
-                           attribute: .trailing,
-                           multiplier: 1,
-                           constant: -50).isActive = true
-        
-        NSLayoutConstraint(item: darkButton,
-                           attribute: .top,
-                           relatedBy: .equal,
-                           toItem: label.safeAreaLayoutGuide,
-                           attribute: .top,
-                           multiplier: 1,
-                           constant: 100).isActive = true
-        NSLayoutConstraint(item: darkButton,
-                           attribute: .leading,
-                           relatedBy: .equal,
-                           toItem: label.safeAreaLayoutGuide,
-                           attribute: .leading,
-                           multiplier: 1,
-                           constant: 25).isActive = true
-        NSLayoutConstraint(item: darkButton,
-                           attribute: .trailing,
-                           relatedBy: .equal,
-                           toItem: label.safeAreaLayoutGuide,
-                           attribute: .trailing,
-                           multiplier: 1,
-                           constant: -75).isActive = true
-        
-        NSLayoutConstraint(item: blueButton,
-                           attribute: .top,
-                           relatedBy: .equal,
-                           toItem: label.safeAreaLayoutGuide,
-                           attribute: .top,
-                           multiplier: 1,
-                           constant: 100).isActive = true
-        NSLayoutConstraint(item: blueButton,
-                           attribute: .top,
-                           relatedBy: .equal,
-                           toItem: label.safeAreaLayoutGuide,
-                           attribute: .top,
-                           multiplier: 1,
-                           constant: 75).isActive = true
-        NSLayoutConstraint(item: blueButton,
-                           attribute: .top,
-                           relatedBy: .equal,
-                           toItem: label.safeAreaLayoutGuide,
-                           attribute: .top,
-                           multiplier: 1,
-                           constant: -25).isActive = true
+        blueButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 50).isActive = true
+        blueButton.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 100).isActive = true
+        blueButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        blueButton.widthAnchor.constraint(equalToConstant: 70).isActive = true
         
     }
     
