cd5440f8e6d037df961f66b157fdab7e0d373ed8
Chad Parker <chad@chadparker.com>
2020-04-08 19:01:24 -0700

    Implement CollectionView FlowLayout Delegate

--- PhotoCollection/PhotoCollection/View Controllers/PhotoCollectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/PhotoCollectionViewController.swift
@@ -87,3 +87,19 @@ class PhotoCollectionViewController: UICollectionViewController {
         }
     }
 }
+
+extension PhotoCollectionViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionViewLayout.collectionViewContentSize.width
+        return CGSize(width: width / 3, height: width / 3)
+    }
+}
