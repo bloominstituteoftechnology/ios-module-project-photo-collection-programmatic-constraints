01fb7c338d8ce464ee4a6497e31beee8ea7f8c4c
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-04-13 13:31:15 -0700

    added constraints to image and label

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -9,8 +9,38 @@
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
-
+    
+    private var imageView = UIImageView()
+    private var imageLabel = UILabel()
     var photo: Photo?
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+         setUpSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func setUpSubviews() {
+        // created image view and label for the photo
+        //add subviews
+        addSubview(imageView)
+        addSubview(imageLabel)
+        // creating constraints
+        
+        let imageXView = NSLayoutConstraint(item: imageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 6)
+        let imageYView = NSLayoutConstraint(item: imageView, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1.0, constant: 6)
+        let imageWidth = NSLayoutConstraint(item: imageView, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1.0, constant: -6)
+          let imageHeightConstraint = NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal, toItem: imageView, attribute: .width, multiplier: 1.0, constant: 0)
+        
+        // activate constraints
+        NSLayoutConstraint.activate([imageXView, imageYView, imageWidth, imageHeightConstraint])
+        
+        
+        
+    }
     
     
 }
