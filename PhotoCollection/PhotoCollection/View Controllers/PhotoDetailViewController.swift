7fb2a70e3ac9ba230b5e47c381e965275bb75a8b
Shawn <shawn.james@me.com>
2020-04-13 22:38:33 -0400

    add setUpSubviews

--- PhotoCollection/PhotoCollection/View Controllers/PhotoDetailViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/PhotoDetailViewController.swift
@@ -11,8 +11,8 @@ import Photos
 
 class PhotoDetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    var imageView: UIImageView!
-    var titleTextField: UITextField!
+    private var imageView: UIImageView!
+    private var titleTextField: UITextField!
     
     var photo: Photo?
     var photoController: PhotoController?
@@ -23,6 +23,7 @@ class PhotoDetailViewController: UIViewController, UIImagePickerControllerDelega
         
         setTheme()
         updateViews()
+        setUpSubviews()
     }
     
     // MARK: - UIImagePickerControllerDelegate
@@ -61,7 +62,7 @@ class PhotoDetailViewController: UIViewController, UIImagePickerControllerDelega
         }
     }
     
-    private func savePhoto() {
+    @objc private func savePhoto() {
         
         guard let image = imageView.image,
             let imageData = image.pngData(),
@@ -116,4 +117,57 @@ class PhotoDetailViewController: UIViewController, UIImagePickerControllerDelega
         
         view.backgroundColor = backgroundColor
     }
+    
+    private func setUpSubviews() {
+//  - Add an `UIImageView` to the view controller's `view`. You can leave its image empty in this function.
+        
+        let imageView = UIImageView()
+        view.addSubview(imageView)
+        
+//        - Add a `UIButton` to the view controller's `view`. Set its title to "Add Image" and add a target to it that calls
+//        the `addImage` function that already exists in this view controller.
+        
+        let addButton = UIButton(type: .system)
+        addButton.setTitle("Add Image", for: .normal)
+        addButton.addTarget(self, action: #selector(done), for: .touchUpInside)
+        view.addSubview(addButton)
+        
+//        - Add a `UITextField` to the view controller's `view`. Set its `placeholder` to something like "Give this photo a title:". You can leave its `text` empty.
+        
+        let titleTextField = UITextField()
+        titleTextField.placeholder = "Enter photo title..."
+        view.addSubview(titleTextField)
+        
+//        - Constrain the above UI elements **using only `NSLayoutAnchor`s**.
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        addButton.translatesAutoresizingMaskIntoConstraints = false
+        titleTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: 10).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1.0).isActive = true
+        
+        addButton.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20).isActive = true
+        addButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        addButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 20).isActive = true
+        
+        titleTextField.topAnchor.constraint(equalTo: addButton.bottomAnchor, constant: 20).isActive = true
+        titleTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        titleTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 20).isActive = true
+        
+//        - Create a `UIBarButtonItem`. Set its title to "Save Photo". Set its action as the `savePhoto` method that already exists in this view controller. Then, set it as the navigation item's right bar button item. (the documentation on `UINavigationItem` is your friend here). **Note:** a view controller subclass already has a `navigationItem` property built-in.
+        
+        let saveButton = UIBarButtonItem()
+        saveButton.title = "Save Photo"
+        saveButton.action = #selector(savePhoto)
+        navigationItem.rightBarButtonItem = saveButton
+    }
+    
+    // MARK: - Objective C
+    @objc func done() {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
