3af8650c714e3385f40730e35cdfcc7745674bb1
BrianRouse <brouse3815@me.com>
2020-05-11 17:06:03 -0400

    Unit 2 Week 2 Mod 1 Programmatic Contraints

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -9,8 +9,49 @@
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    let imageView = UIImageView()
+    
+    let titleLabel = UILabel()
+
 
     var photo: Photo?
+    override func awakeFromNib() {
+    setUpSubviews()
+
+    }
+
+func setUpSubviews() {
+        
+        self.contentView.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleToFill
+        
+        self.contentView.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textAlignment = .center
+        titleLabel.textColor = UIColor.black
+        
+        self.setupConstraint()
+    }
     
+    private func setupConstraint() {
+        self.contentView.addConstraint(NSLayoutConstraint(item: imageView, attribute: .trailing, relatedBy: .equal, toItem: self.contentView, attribute: .trailing, multiplier: 1, constant: -10))
+        self.contentView.addConstraint(NSLayoutConstraint(item: imageView, attribute: .leading, relatedBy: .equal, toItem: self.contentView, attribute: .leading, multiplier: 1, constant: 10))
+        self.contentView.addConstraint(NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 100))
+        self.contentView.addConstraint(NSLayoutConstraint(item: imageView, attribute: .top, relatedBy: .equal, toItem: self.contentView, attribute: .topMargin, multiplier: 1, constant: 10))
+        
+        self.contentView.addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .trailing, relatedBy: .equal, toItem: self.contentView, attribute: .trailing, multiplier: 1, constant: -10))
+        self.contentView.addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .leading, relatedBy: .equal, toItem: self.contentView, attribute: .leading, multiplier: 1, constant: 10))
+        self.contentView.addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 24.0))
+        self.contentView.addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .bottom, relatedBy: .equal, toItem: self.contentView, attribute: .bottomMargin, multiplier: 1, constant: 0.0))
+    }
     
+    func updateViews() {
+        if let photo = self.photo {
+            self.titleLabel.text = photo.title
+            self.imageView.image = UIImage(data: photo.imageData)
+        }
+    }
 }
+
