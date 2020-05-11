0cd4c08cfbd34d1030663de1f28b44b5bb26e2f4
Nonye <nonye@Nonyes-MacBook-Air.local>
2020-05-11 12:05:08 -0700

    Part II - cell set up complete

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -11,6 +11,69 @@ import UIKit
 class PhotoCollectionViewCell: UICollectionViewCell {
 
     var photo: Photo?
+    //MARK: - Programmatic Outlets
+    private var imageView = UIImageView()
+    private var photoTitle = UILabel()
     
-    
+    //MARK: - INITIALIZER 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubviews()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) is not supported")
+    }
+    //MARK: - UPDATE VIEWS FUNCTION
+    func updateViews() {
+        guard let photo = photo else { return }
+        photoTitle.text = photo.title
+        imageView.image = UIImage(data: photo.imageData)
+    }
+    //MARK: - SUBVIEWS FUNCTION
+    func setUpSubviews() {
+        //1. Create
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        addSubview(imageView)
+        
+        //Create/activate constraints
+        //MARK: - Y CONSTRAINTS
+        NSLayoutConstraint(item: imageView,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+        
+        //MARK: - X CONSTRAINTS
+        NSLayoutConstraint(item: imageView,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .leading,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+        
+        //MARK: - WIDTH CONSTRAINTS
+        NSLayoutConstraint(item: imageView,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .trailing,
+                           multiplier: 1,
+                           constant: -4).isActive = true
+        
+        //MARK: HEIGHT CONSTRAINTS
+        NSLayoutConstraint(item: imageView,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: imageView,
+                           attribute: .width,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+         
+        
+        
+    }
 }
