e6edde845f3f32492cd03ae97cd176acaf128206
Stephanie Ballard <user.name>
2020-05-12 11:56:58 -0700

    cell set up

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -15,8 +15,17 @@ class PhotoCollectionViewCell: UICollectionViewCell {
     
     var photo: Photo? {
         didSet {
-            
+            updateViews()
         }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) is not supported")
     }
     
     func setUpSubviews() {
@@ -26,6 +35,7 @@ class PhotoCollectionViewCell: UICollectionViewCell {
         
         addSubview(imageView)
         
+        // this is the y
         NSLayoutConstraint(item: imageView,
                            attribute: .top,
                            relatedBy: .equal,
@@ -34,6 +44,7 @@ class PhotoCollectionViewCell: UICollectionViewCell {
                            multiplier: 1,
                            constant: 20).isActive = true
         
+        // this is the x
         NSLayoutConstraint(item: imageView,
                            attribute: .leading,
                            relatedBy: .equal,
@@ -42,6 +53,7 @@ class PhotoCollectionViewCell: UICollectionViewCell {
                            multiplier: 1,
                            constant: 20).isActive = true
         
+        // this is the width
         NSLayoutConstraint(item: imageView,
                            attribute: .trailing,
                            relatedBy: .equal,
@@ -50,6 +62,7 @@ class PhotoCollectionViewCell: UICollectionViewCell {
                            multiplier: 1,
                            constant: 20).isActive = true
         
+        // this is the height
         NSLayoutConstraint(item: imageView,
                            attribute: .height,
                            relatedBy: .equal,
@@ -59,5 +72,25 @@ class PhotoCollectionViewCell: UICollectionViewCell {
                            constant: 0).isActive = true
         
         // Create a Label
+        
+        photoTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        photoTitleLabel.textAlignment = .center
+        
+        addSubview(photoTitleLabel)
+        
+        // this is the y
+        photoTitleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
+    
+        // this is the x
+        photoTitleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 4).isActive = true
+        
+        // this is the width
+        photoTitleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -4).isActive = true
+    }
+    
+    private func updateViews() {
+        guard let photo = photo else { return }
+        imageView.image = UIImage(data: photo.imageData)
+        photoTitleLabel.text = photo.title
     }
 }
