3c7a6a0a939707bdcb21828fd4fb9281fdc69ac8
Craig Belinfante <craigbasketball@aol.com>
2020-07-23 18:21:49 -0700

    fixed storyboard

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -10,7 +10,54 @@ import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
 
-    var photo: Photo?
+    static let reuseIdentifier = "PhotoCell"
     
+    private var imageView = UIImageView()
+    private var labelTitle = UILabel()
+    
+    var photo: Photo? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder: \(coder) is not supported")
+    }
+    
+    private func updateViews() {
+        guard let photo = photo else {return}
+        imageView.image = UIImage(data: photo.imageData)
+        labelTitle.text = photo.title
+        
+    }
+    
+    private func setUpSubViews() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint(item: imageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 4).isActive = true
+        NSLayoutConstraint(item: imageView, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: 4).isActive = true
+        NSLayoutConstraint(item: imageView, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: -4).isActive = true
+        NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal, toItem: imageView, attribute: .width, multiplier: 1, constant: 0).isActive = true
+        
+        //label
+        labelTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelTitle.textAlignment = .center
+        
+        addSubview(labelTitle)
+        
+        NSLayoutConstraint(item: labelTitle, attribute: .top, relatedBy: .equal, toItem: imageView, attribute: .bottom, multiplier: 1, constant: 4).isActive = true
+        NSLayoutConstraint(item: labelTitle, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: 2).isActive = true
+        NSLayoutConstraint(item: labelTitle, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: -2).isActive = true
+        
+    }
     
 }
